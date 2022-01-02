8fbd3b772238db30d1e5379b7d2ceb7ef83590b2
lfernandes00 <luis_santos_fernandes@hotmail.com>
2022-01-02 21:42:02 +0000

    Start of the navbar

---
+++ luis-fernandes/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <b-navbar id="nav" class="navbar-menu has-background-one" >
+    <b-navbar-nav id="navItems">
+      <b-nav-item href="#">About Me</b-nav-item>
+
+      <b-nav-item href="#">Skills</b-nav-item>
+
+      <b-nav-item href="#">Projects</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+name:"NavBar"
+}
+</script>
+
+<style>
+#nav {
+    min-height: 3.25rem;
+    background-image: linear-gradient(to bottom right, rgb(2, 2, 100), rgb(22, 22, 99));
+    color: white!important;
+}
+#navItems {
+    margin-left: 3.9rem;
+    margin-right: 3.9rem;
+}
+
+</style>
