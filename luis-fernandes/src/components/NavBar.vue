1917826b4aa5bcddf2c7b8a08dfb0c91fcfa0467
lfernandes00 <luis_santos_fernandes@hotmail.com>
2022-01-03 00:06:36 +0000

    work on navbar

--- luis-fernandes/src/components/NavBar.vue
+++ luis-fernandes/src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-  <b-navbar id="nav" class="navbar-menu has-background-one" >
-    <b-navbar-nav id="navItems">
-      <b-nav-item href="#">About Me</b-nav-item>
-
-      <b-nav-item href="#">Skills</b-nav-item>
-
-      <b-nav-item href="#">Projects</b-nav-item>
-    </b-navbar-nav>
-  </b-navbar>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">About Me</a>
+      <a class="nav-item nav-link" href="#">Skills</a>
+      <a class="nav-item nav-link" href="#">Projects</a>
+    </div>
+  </div>
+</nav>
 </div>
 </template>
 
@@ -19,14 +19,20 @@ name:"NavBar"
 </script>
 
 <style>
-#nav {
+nav {
     min-height: 3.25rem;
-    background-image: linear-gradient(to bottom right, rgb(2, 2, 100), rgb(22, 22, 99));
-    color: white!important;
+    background-image: linear-gradient(to bottom right, rgb(20, 30, 117), rgb(64, 81, 231));
 }
-#navItems {
+#navbarNavAltMarkup {
     margin-left: 3.9rem;
     margin-right: 3.9rem;
+}
+nav .navbar-nav .nav-item {
+  color: white !important;
+  font-weight: bold;
+}
+.nav-item.nav-item.nav-item a:hover {
+  color: black
 }
 
 </style>
